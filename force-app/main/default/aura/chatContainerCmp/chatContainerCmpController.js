013131edb6719744b508bcd083ced00d4a17e495
KaBleeth <kableeth@gmail.com>
2023-05-09 21:32:17 -0500

    now it logs when is opened. Need it to create a conversation

--- force-app/main/default/aura/chatContainerCmp/chatContainerCmpController.js
+++ force-app/main/default/aura/chatContainerCmp/chatContainerCmpController.js
@@ -1,5 +1,20 @@
+// chatContainerCmpController.js
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit: function (component, event, helper) {
+        var utilityAPI = component.find('utilitybar');
+        
+        utilityAPI.getAllUtilityInfo().then(function (response) {
+            if (typeof response !== 'undefined') {
+                component.set('v.hasUtilityBar', true);
+                
+                // log the utility ids
+                for (var i = 0; i < response.length; i++) {
+                    console.log(response[i].id);
+                }
+                
+            } else {
+                component.set('v.hasUtilityBar', false);
+            }
+        });
+    },
 })
--- force-app/main/default/aura/chatContainerCmp/chatContainerCmpHelper.js
+++ force-app/main/default/aura/chatContainerCmp/chatContainerCmpHelper.js
@@ -1,16 +1,3 @@
+// chatContainerCmpController.js
 ({
-  registerUtilityClickHandler: function(component, event, helper){
-        var utilityBarAPI = component.find("utilitybar");
-	  var eventHandler = function(response){
-            console.log(response);
-        };
-        
-        utilityBarAPI.onUtilityClick({ 
-               eventHandler: eventHandler 
-        }).then(function(result){
-            console.log(result);
-        }).catch(function(error){
-        	console.log(error);
-        });
-    }
 })
